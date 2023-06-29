4ce79b140ecc98e7d10bc2320a0b08e72ed62479
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-06-29 02:24:18 -0500

    crud de categoria, marca, modelo and articulo

--- entornovirtual/proyecto/Almacen/urls.py
+++ entornovirtual/proyecto/Almacen/urls.py
@@ -7,5 +7,29 @@ urlpatterns=[
     path("",views.login_usuario),
     path("RegistroUsuario",views.registrar_usuario),
 
+    path('marca',views.marca,name='marca'),
+    path('marca/registrarmarca',views.registrarmarca),
+    path('marca/edicionmarca/<codigo>',views.edicionmarca),
+    path('marca/updatemarca/<codigo>',views.updatemarca),
+    path('marca/eliminarmarca/<codigo>',views.eliminarmarca),
+
+    path('modelo',views.modelo,name='modelo'),
+    path('modelo/registrarmodelo',views.registrarmodelo),
+    path('modelo/edicionmodelo/<codigo>',views.edicionmodelo),
+    path('modelo/updatemodelo/<codigo>',views.updatemodelo),
+    path('modelo/eliminarmodelo/<codigo>',views.eliminarmodelo),
+
+    path('categoria',views.categoria,name='categoria'),
+    path('categoria/registrarcategoria',views.registrarcategoria),
+    path('categoria/edicioncategoria/<codigo>',views.edicioncategoria),
+    path('categoria/updatecategoria/<codigo>',views.updatecategoria),
+    path('categoria/eliminarcategoria/<codigo>',views.eliminarcategoria),
+
+    path('articulo',views.articulo,name='articulo'),
+    path('articulo/registrararticulo',views.registrararticulo),
+    path('articulo/edicionarticulo/<codigo>',views.edicionarticulo),
+    path('articulo/updatearticulo/<codigo>',views.updatearticulo),
+    path('articulo/eliminararticulo/<codigo>',views.eliminararticulo),
+
     path("Inicio",views.index)
     ]
--- entornovirtual/proyecto/Almacen/views.py
+++ entornovirtual/proyecto/Almacen/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect, render
-from .models import Usuario
+from .models import Usuario, Marca, Modelo, Categoria, Articulo, Orden_Ingreso, Orden_Pedido, Orden_Salida
 from pyexpat.errors import messages
 
 template_login="usuario/login.html"
 template_registro = "usuario/registro.html"
-template_index="inicio.html"
+#template_index="inicio.html"
 ##VISTAS
 
 #Acciones
@@ -18,18 +18,26 @@ template_index="inicio.html"
 #acciones/pedido/edit_salida.html
 
 ##Productos 
+template_gestion_articulo="producto/GestionProducto.html"
+template_edit_articulo="producto/GestionProducto.html"
 #producto/GestionProducto.html
 #producto/edit_producto.html
 
 ### Categoria
+template_gestion_categoria="producto/GestionCategoria.html"
+template_edit_categoria="producto/categoria/edit_categoria.html"
 #producto/categoria/GestionCategoria.html
 #producto/categoria/editCategoria.html
 
 ### Marca
+template_gestion_marca="producto/GestionMarca.html"
+template_edit_marca="producto/edit_marca.html"
 #producto/marca/GestionMarca.html
 #producto/marca/edit_marca.html
 
 ### Modelo
+template_gestion_modelo="producto/modelo/GestionModelo.html"
+template_edit_modelo="producto/modelo/edit_modelo.html"
 #producto/modelo/GestionModelo.html
 #producto/modelo/edit_modelo.html
 
@@ -46,7 +54,7 @@ def login_usuario(request):
         
         if (user):
             context = {"msg":"usuario correcto","usuario":user}
-            return render(request,template_index,context)
+            return render(request,template_gestion_articulo,context)
         else:
             message = {"msg":"datos invalidos"}
             return render(request,template_login,message)
@@ -80,6 +88,161 @@ def registrar_usuario(request):
         return render(request,template_registro)
     
 
-#Gestion a la tabla cursos
-def index(request):
-    return render(request,template_index)
+#Gestion a la tabla marca
+def marca(request):
+    marcas = Marca.objects.all()
+    context = {"marcas":marcas}
+    return render(request, template_gestion_marca,context)
+
+def edicionmarca(request, codigo):
+    marca = Marca.objects.get(codigo = codigo)
+    context = {"marca":marca}
+    return render(request, template_edit_marca,context)
+
+def updatemarca(request, codigo):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+
+    marca = Marca.objects.get(codigo = codigo)
+    marca.codigo = codigo
+    marca.nombre = nombre
+    marca.save()
+    return redirect("/marca")
+
+def registrarmarca(request):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+
+    marca = Marca.objects.create(codigo=codigo,nombre=nombre)
+    return redirect("/marca")
+
+def eliminarmarca(request, codigo):
+    marca = Marca.objects.get(codigo = codigo)
+    marca.delete()
+    return redirect("/marca")
+
+
+#Gestion a la tabla modelo
+def modelo(request):
+    modelos = Modelo.objects.all()
+    context = {"modelos":modelos}
+    return render(request, template_gestion_modelo,context)
+
+def edicionmodelo(request, codigo):
+    modelo = Modelo.objects.get(codigo = codigo)
+    context = {"modelo":modelo}
+    return render(request, template_edit_modelo,context)
+
+def updatemodelo(request, codigo):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+
+    modelo = Modelo.objects.get(codigo = codigo)
+    modelo.codigo = codigo
+    modelo.nombre = nombre
+    modelo.save()
+    return redirect("/modelo")
+
+def registrarmodelo(request):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+
+    modelo = Modelo.objects.create(codigo=codigo,nombre=nombre)
+    return redirect("/modelo")
+
+def eliminarmodelo(request, codigo):
+    modelo = Modelo.objects.get(codigo = codigo)
+    modelo.delete()
+    return redirect("/modelo")
+
+
+#Gestion a la tabla categoria
+def categoria(request):
+    categorias = Categoria.objects.all()
+    context = {"categorias":categorias}
+    return render(request, template_gestion_categoria,context)
+
+def edicioncategoria(request, codigo):
+    categoria = Categoria.objects.get(codigo = codigo)
+    context = {"categoria":categoria}
+    return render(request, template_edit_categoria,context)
+
+def updatecategoria(request, codigo):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+    descripcion = request.POST["txtDescripcion"]
+
+    categoria = Categoria.objects.get(codigo = codigo)
+    categoria.codigo = codigo
+    categoria.nombre = nombre
+    categoria.descripcion = descripcion
+    categoria.save()
+    return redirect("/categoria")
+
+def registrarcategoria(request):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+    descripcion = request.POST["txtDescripcion"]
+
+    categoria = Categoria.objects.create(codigo=codigo,nombre=nombre,descripcion=descripcion)
+    return redirect("/categoria")
+
+def eliminarcategoria(request, codigo):
+    categoria = Categoria.objects.get(codigo = codigo)
+    categoria.delete()
+    return redirect("/categoria")
+
+
+#Gestion a la tabla articulo
+def articulo(request):
+    articulos = Articulo.objects.all()
+    context = {"articulos":articulos}
+    return render(request, template_gestion_articulo,context)
+
+def edicionarticulo(request, codigo):
+    articulo = Articulo.objects.get(codigo = codigo)
+    context = {"articulo":articulo}
+    return render(request, template_edit_articulo,context)
+
+def updatearticulo(request, codigo):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+    stock = request.POST["txtStock"]
+    modelo = request.POST["txtModelo"]
+    marca = request.POST["txtMarca"]
+    categoria = request.POST["txtCategoria"]
+
+    articulo = Articulo.objects.get(codigo = codigo)
+    articulo.codigo = codigo
+    articulo.nombre = nombre
+    articulo.stock = stock
+    modelo_fk = Modelo.objects.get(codigo = modelo)
+    articulo.modelo = modelo_fk
+    marca_fk = Marca.objects.get(codigo = marca)
+    articulo.marca = marca_fk
+    categoria_fk = Categoria.objects.get(codigo = categoria)
+    articulo.categoria = categoria_fk
+
+    articulo.save()
+    return redirect("/articulo")
+
+
+def registrararticulo(request):
+    codigo = request.POST["txtCodigo"]
+    nombre = request.POST["txtNombre"]
+    stock = request.POST["txtStock"]
+    modelo = request.POST["txtModelo"]
+    marca = request.POST["txtMarca"]
+    categoria = request.POST["txtCategoria"]
+
+    modelo_fk = Modelo.objects.get(codigo = modelo)
+    marca_fk = Marca.objects.get(codigo = marca)
+    categoria_fk = Categoria.objects.get(codigo = categoria)
+
+    articulo = Articulo.objects.create(codigo=codigo,nombre=nombre,stock=stock,modelo=modelo_fk,marca=marca_fk,categoria=categoria_fk)
+    return redirect("/articulo")
+
+def eliminararticulo(request, codigo):
+    articulo = Articulo.objects.get(codigo = codigo)
+    articulo.delete()
+    return redirect("/articulo")
