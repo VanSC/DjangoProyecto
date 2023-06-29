ef4e39e882df6357ce7db6f6c1b10fe160d9ac8a
VanSC <vanessa.choque@tecsup.edu.pe>
2023-06-29 02:03:29 -0500

    Nombre actualizado

--- entornovirtual/proyecto/Almacen/admin.py
+++ entornovirtual/proyecto/Almacen/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Usuario, Categoria, Articulo, Marca, Modelo, Orden_Salida, Orden_Ingreso, Orden_Pedido
 
-# Register your models here.
+##user admin
+##pass admin
+
+class AdminUsuario(admin.ModelAdmin):
+    list_display=("codigo","nombreusuario","nombre","apellido","email","contraseña")
+
+class AdminMarca(admin.ModelAdmin):
+    list_display=('codigo','nombre')
+
+class AdminModelo(admin.ModelAdmin):
+    list_display=('codigo','nombre')
+
+class AdminCategoria(admin.ModelAdmin):
+    list_display=('codigo','nombre','descripcion')
+
+class AdminArticulo(admin.ModelAdmin):
+    list_display=('codigo','nombre','stock','codigo_Modelo','codigo_Marca','codigo_Categoria')
+
+class AdminOrdenIngreso(admin.ModelAdmin):
+    list_display=('codigo','codigo_Articulo','cant_Art_Ingresados','fecha_Ingreso','codigo_Usuario')
+
+class AdminOrdenPedido(admin.ModelAdmin):
+    list_display=('codigo','codigo_Articulo','cant_Solicitada','fecha_Solicitud','codigo_Usuario')
+
+class AdminOrdenSalida(admin.ModelAdmin):
+    list_display=('codigo','codigo_Articulo','cant_Art_Salida','fecha_Salida','codigo_Usuario')
+
+admin.site.register(Usuario,AdminUsuario)
+admin.site.register(Categoria,AdminCategoria)
+admin.site.register(Articulo,AdminArticulo)
+admin.site.register(Marca,AdminMarca)
+admin.site.register(Modelo,AdminModelo)
+admin.site.register(Orden_Ingreso,AdminOrdenIngreso)
+admin.site.register(Orden_Pedido,AdminOrdenPedido)
+admin.site.register(Orden_Salida,AdminOrdenSalida)
+
+
+
--- entornovirtual/proyecto/Almacen/models.py
+++ entornovirtual/proyecto/Almacen/models.py
@@ -9,18 +9,30 @@ class Usuario(models.Model):
     email = models.EmailField(max_length=40)
     contraseña = models.CharField(max_length=12)
 
+    def __str__(self):
+        return self.nombreusuario
+
 class Marca(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     nombre=models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre
 
 class Modelo(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     nombre=models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.nombre
+
 class Categoria(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     nombre=models.CharField(max_length=60)
     descripcion=models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.nombre
 
 class Articulo(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
@@ -30,12 +42,18 @@ class Articulo(models.Model):
     codigo_Marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     codigo_Categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nombre
+
 class Orden_Ingreso(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     codigo_Articulo=models.ForeignKey(Articulo, on_delete=models.CASCADE)
     cant_Art_Ingresados=models.IntegerField()
     fecha_Ingreso=models.DateTimeField()
     codigo_Usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.codigo_Articulo
 
 class Orden_Pedido(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
@@ -44,10 +62,16 @@ class Orden_Pedido(models.Model):
     fecha_Solicitud=models.DateTimeField()
     codigo_Usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.codigo_Articulo
+
 class Orden_Salida(models.Model):
     codigo=models.CharField(primary_key=True, max_length=6)
     codigo_Articulo=models.ForeignKey(Articulo, on_delete=models.CASCADE)
     cant_Art_Salida=models.IntegerField()
     fecha_Salida=models.DateTimeField()
     codigo_Usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.codigo_Articulo
 
